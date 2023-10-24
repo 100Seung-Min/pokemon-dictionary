8437ba148bf09f553aff306a776ae409b99c2c7a
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-10-24 14:06:37 +0900

    뷰페이저 인디케이터 추가

--- feature/pokemon/src/main/kotlin/com/pokemon/feature/pokemon/detail/DetailScreen.kt
+++ feature/pokemon/src/main/kotlin/com/pokemon/feature/pokemon/detail/DetailScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -22,6 +23,7 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -33,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -151,17 +154,51 @@ fun DetailScreen(
                 val moveDescriptionPagerState = rememberPagerState {
                     it.flavorList.size
                 }
-                HorizontalPager(state = moveDescriptionPagerState) { index ->
-                    Text(text = it.flavorList[index])
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    HorizontalPager(state = moveDescriptionPagerState) { index ->
+                        Text(text = it.flavorList[index])
+                    }
+                    LazyRow {
+                        items(it.flavorList.size) {
+                            Box(
+                                modifier = Modifier
+                                    .padding(horizontal = 3.dp)
+                                    .size(5.dp)
+                                    .background(
+                                        if (it == moveDescriptionPagerState.currentPage) Color.Black else Color.Gray,
+                                        CircleShape
+                                    )
+                            )
+                        }
+                    }
                 }
             }
         }
         Text(text = "상세 설명")
-        val descriptionPagerState = rememberPagerState {
-            state.flavorList.size
-        }
-        HorizontalPager(state = descriptionPagerState) {
-            Text(text = state.flavorList[it])
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val descriptionPagerState = rememberPagerState {
+                state.flavorList.size
+            }
+            HorizontalPager(state = descriptionPagerState) {
+                Text(text = state.flavorList[it])
+            }
+            LazyRow {
+                items(state.flavorList.size) {
+                    Box(
+                        modifier = Modifier
+                            .padding(horizontal = 3.dp)
+                            .size(5.dp)
+                            .background(
+                                if (it == descriptionPagerState.currentPage) Color.Black else Color.Gray,
+                                CircleShape
+                            )
+                    )
+                }
+            }
         }
     }
 }

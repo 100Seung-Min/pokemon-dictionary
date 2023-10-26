549b5b2ae75b8c86a95b2fa886267e440df2fa4c
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-10-26 10:25:40 +0900

    바텀네비게이션 컨트롤 기능 추가

--- feature/main/src/main/kotlin/com/pokemon/feature/main/navigation/BottomNavigation.kt
+++ feature/main/src/main/kotlin/com/pokemon/feature/main/navigation/BottomNavigation.kt
@@ -1,10 +1,17 @@
 package com.pokemon.feature.main.navigation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -24,32 +31,46 @@ fun PokemonBottomNavigation(
         BottomNavigationItem.Academy,
         BottomNavigationItem.Setting
     )
-    BottomNavigation(
-        modifier = Modifier.height(40.dp),
-        backgroundColor = Color.LightGray,
-        contentColor = Color.Black
+    AnimatedVisibility(
+        visible = itemList.map { it.route }.contains(currentRoute),
+        enter = slideInVertically(
+            initialOffsetY = { 0 },
+            animationSpec = tween(durationMillis = 500)
+        ),
+        exit = slideOutVertically(
+            targetOffsetY = { 0 },
+            animationSpec = tween(durationMillis = 500)
+        )
     ) {
-        itemList.forEach { item ->
-            BottomNavigationItem(
-                icon = {
-                    PokemonIcon(
-                        icon = item.icon,
-                        alpha = if (currentRoute == item.route) 1F else 0.4F
-                    )
-                },
-                selectedContentColor = Color.Black,
-                unselectedContentColor = Color.Gray,
-                selected = currentRoute == item.route,
-                onClick = {
-                    navController.navigate(item.route) {
-                        navController.graph.startDestinationRoute?.let { screenRoute ->
-                            popUpTo(screenRoute)
+        BottomNavigation(
+            modifier = Modifier.height(45.dp),
+            backgroundColor = Color.LightGray,
+            contentColor = Color.Black
+        ) {
+            itemList.forEach { item ->
+                BottomNavigationItem(
+                    icon = {
+                        PokemonIcon(
+                            icon = item.icon,
+                            alpha = if (currentRoute == item.route) 1F else 0.4F
+                        )
+                    },
+                    selectedContentColor = Color.Black,
+                    unselectedContentColor = Color.Gray,
+                    selected = currentRoute == item.route,
+                    onClick = {
+                        navController.navigate(item.route) {
+                            navController.graph.startDestinationRoute?.let { screenRoute ->
+                                popUpTo(screenRoute) {
+                                    saveState = true
+                                }
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
                     }
-                }
-            )
+                )
+            }
         }
     }
 }
